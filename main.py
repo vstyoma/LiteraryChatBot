a01dd0f6a1f95c6cc4e7b5f684c0d2a0de2e3834
vstyoma <testiiing44679@gmail.com>
2023-07-19 23:44:20 +0300

    Markdown + FSM

--- main.py
+++ main.py
@@ -3,7 +3,9 @@ import requests
 import logging
 
 from aiogram import Bot, Dispatcher, executor, types
-
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from aiogram.dispatcher import FSMContext
+from aiogram.dispatcher.filters.state import State, StatesGroup
 
 API_TOKEN = '5944894990:AAF1ivQiWLxnRbG6_E0sapKwlUbZwoF78zw'
 
@@ -12,80 +14,107 @@ logging.basicConfig(level=logging.INFO)
 
 # Initialize bot and dispatcher
 bot = Bot(token=API_TOKEN)
-dp = Dispatcher(bot)
 
+
+storage = MemoryStorage()
+dp = Dispatcher(bot, storage=storage)
+
+class Form(StatesGroup):
+    book_name = State()
 
 @dp.message_handler(commands=['start', 'help'])
 async def send_welcome(message: types.Message):
 
     await message.reply("Привет!🤗 \n\nЯ -  Литературный Ботик!📚 \n\nНиже мой список команд: \n\n/findinfo - поиск информации о ведённой вами книге. \n Скоро...")
 
+
+
 @dp.message_handler(commands=['findinfo',])
 async def find_information(message: types.Message):
-     await message.answer("Введите название книги:")
-
-     url = "https://www.googleapis.com/books/v1/volumes"
-
-     query = str(message.text)
-
-     params = {"q": query, "maxResults": 1}
-     response = requests.get(url, params=params).json()
-
-     for book in response['items']:
+     await Form.book_name.set()
+     await message.answer("Введите название книги. Введите /cancel для отмены команды.")
 
 
-          volume = book["volumeInfo"]
-          title = volume["title"]
-          author = volume["authors"]
 
-          published = volume.get("publishedDate", "год издания неизвестен")
-          description = volume.get("description", "описание отсутствует")
+@dp.message_handler(state='*', commands=['cancel'])
+async def cancel_handler(message: types.Message, state: FSMContext):
+    current_state = await state.get_state()
+    if current_state is None:
+        return
 
-# url = "https://www.googleapis.com/books/v1/volumes"
-# query = "1984"
-#
-# params = {"q": query, "maxResults": 1}
-# response = requests.get(url, params=params).json()
-#
-# for book in response['items']:
-#      volume = book["volumeInfo"]
-#      title = volume["title"]
-#      author = volume["authors"]
-#
-#      published = volume.get("publishedDate", "год издания неизвестен")
-#      description = volume.get("description", "описание отсутствует")
-#
-#
-#
-#
-#      if author:
-#
-#           finalansw = [f"**{title}** ({published})| {author} | {description}"]
-#
-#           string = ''
-#           chars_to_remove = ['[', ']', "'"]
-#
-#           for i in finalansw:
-#                string += str(i)
-#                string += ' '
-#
-#           for char in chars_to_remove:
-#                string = string.replace(char, '')
-#           print(string)
-#
-#      else:
-#           finalansw = [f"**{title}** ({published})| - | {description}"]
-#
-#           string = ''
-#           chars_to_remove = ['[', ']', "'"]
-#
-#           for i in finalansw:
-#                string += str(i)
-#                string += ' '
-#
-#           for char in chars_to_remove:
-#                string = string.replace(char, '')
-#           print(string)
+
+    await state.finish()
+    await message.reply('Отменено.')
+
+
+
+@dp.message_handler(state=Form.book_name)
+async def process_name(message: types.Message, state: FSMContext):
+
+    await state.finish()
+      # <-- Получаем название
+
+
+
+    url = "https://www.googleapis.com/books/v1/volumes"
+
+
+
+
+
+
+
+
+
+
+
+
+    params = {"q": message.text, "maxResults": 1}
+    response = requests.get(url, params=params).json()
+
+    for book in response.get('items', []):
+
+
+        volume = book["volumeInfo"]
+        title = volume["title"]
+        author = volume["authors"]
+
+        published = volume.get("publishedDate", "год издания неизвестен")
+        description = volume.get("description", "описание отсутствует")
+
+        if author:
+
+            finalansw = [f"Название: *{title}* \nГод издательства: *{published}* \nАвтор: *{author}* \nОписание: _{description}_"]
+
+            string = ''
+            chars_to_remove = ['[', ']', "'"]
+
+            for i in finalansw:
+                        string += str(i)
+                        string += ' '
+
+            for char in chars_to_remove:
+                    string = string.replace(char, '')
+            await message.answer(string, parse_mode="Markdown")
+
+        else:
+            finalansw = [f"Название: *{title}* \nГод издательства: *{published}* \nАвтор: *--* \nОписание: _{description}_"]
+
+            string = ''
+            chars_to_remove = ['[', ']', "'"]
+
+            for i in finalansw:
+                    string += str(i)
+                    string += ' '
+
+            for char in chars_to_remove:
+                    string = string.replace(char, '')
+            await message.answer(string, parse_mode="Markdown")
+
+
+
+
+
 if __name__ == '__main__':
     executor.start_polling(dp, skip_updates=True)
 
